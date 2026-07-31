f216c5f399caf76222d2a0c20222e65b9735cbfa
paidiudhay2024-create <paidi.udhay2024@vitstudent.ac.in>
2026-07-31 06:41:28 +0530

    verify python once

---
+++ Verification/verify.py
@@ -0,0 +1,13 @@
+print("================================")
+print("Cloud Assisted HIL Verification")
+print("================================")
+
+expected = 255
+received = 255
+
+if expected == received:
+    print("Verification Result : PASS")
+else:
+    print("Verification Result : FAIL")
+
+print("Verification Completed Successfully")
